2fbe1004b80f82d36e86a1d6fafaa6b763c7b2bc
Nizav <nizav.business@gmail.com>
2024-03-22 11:15:19 +0700

    added tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class=" text-red-300">brother I am alive.</h1>
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <button class="btn">Button</button>
+<button class="btn btn-neutral">Neutral</button>
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-secondary">Secondary</button>
+<button class="btn btn-accent">Accent</button>
+<button class="btn btn-ghost">Ghost</button>
+<button class="btn btn-link">Link</button>
